77cc909ceb8ce1cad85c2b3e7a40b320549164d9
Annalena Feix <annalena.feix@tum.de>
2022-04-08 19:30:23 +0200

    frontend implementation

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import brain from './brain.png'
+
+const Logo = () => {
+	return (
+		<div className='ma4 mt0' style={{ height: '150px', width: '150px' }}>
+			<Tilt className='br2 shadow-2 tilt'>
+      			<div style={{ height: '150px', width: '150px' }} className='pa3'>
+        			<img 
+        				src={brain} 
+        				alt='logo'
+        				style={{ paddingTop: '5px' }}/>
+      			</div>
+    		</Tilt>
+		</div>
+	);
+};
+
+export default Logo;
